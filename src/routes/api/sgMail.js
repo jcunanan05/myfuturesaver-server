bfc22da678aa3dacd28a66cd0ff27cdb29b60d1e
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-04-10 14:30:30 -0400

    Add Docs in /sg-mail route

--- src/routes/api/sgMail.js
+++ src/routes/api/sgMail.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { corsWithOptions } = require('../../controllers/corsController');
 
 /**
  * sgMail route - separated /mail route to /sgMail to separate mailgun with sendgrid. Will migrate later.
  */
 
-router.get('/send', (__, res) => {
+/**
+ * Send Route - send a simple mail. without attachment.
+ * @param {string} to - email send to
+ * @param {string} from - email from
+ * @param {string} subject - email title/subject
+ * @param {string} text - email message
+ * @param {string} [html] - optional html email message
+ */
+
+router.post('/send', (__, res) => {
   res.json({
     message: 'send endpoint hit'
   });
 });
+
+router.options('/send', corsWithOptions());
 
 module.exports = router;
